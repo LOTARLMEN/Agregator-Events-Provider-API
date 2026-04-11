e7d572c5fd3e4660e4aa0ac381f20354e76c4577
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-11 17:05:42 +0200

    create sync meta repo class

---
+++ src/infrastructure/db/repositories/sync_meta.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from .base import BaseRepo
+from src.infrastructure.db.models.sync_meta import SyncMeta
+
+
+class SyncMetaRepo(BaseRepo):
+
+    async def get(self) -> SyncMeta | None:
+        stmt = select(SyncMeta).where(SyncMeta.id == 1)
+
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
