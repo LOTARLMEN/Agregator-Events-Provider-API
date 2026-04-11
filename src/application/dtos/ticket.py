4e0f5740d3629ac50ca803880b48632d5f5fd08c
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-11 17:00:32 +0200

    create pydantic ticket models for fastapi

---
+++ src/application/dtos/ticket.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+
+class TicketResponseSchem(BaseModel):
+
+    id: UUID = Field(alias="ticket_id")
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TicketRequestSchem(BaseModel):
+
+    event_id: UUID
+    first_name: str
+    last_name: str
+    email: EmailStr
+    seat: str
