d7df6d9edd969d69a2a8428103b82af85f8a64a8
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-11 17:02:20 +0200

    fixed event model names column uuid -> id

--- src/infrastructure/db/models/event.py
+++ src/infrastructure/db/models/event.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from .base import Base
 from sqlalchemy import UUID, ForeignKey, DateTime
-from sqlalchemy.orm import Mapped, relationship
-from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 import uuid as uuid_pkg
 from .mixins import TimestampMixin
 from typing import TYPE_CHECKING
@@ -13,7 +12,7 @@ if TYPE_CHECKING:
 
 class Event(Base, TimestampMixin):
 
-    uuid: Mapped[uuid_pkg.UUID] = mapped_column(
+    id: Mapped[uuid_pkg.UUID] = mapped_column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid_pkg.uuid4,
@@ -22,7 +21,7 @@ class Event(Base, TimestampMixin):
     )
     name: Mapped[str] = mapped_column(nullable=False)
     place: Mapped["Place"] = relationship("Place", back_populates="events")
-    place_uuid: Mapped[uuid_pkg.UUID] = mapped_column(ForeignKey("places.uuid"))
+    place_uuid: Mapped[uuid_pkg.UUID] = mapped_column(ForeignKey("places.id"))
     event_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     registration_deadline: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     status: Mapped[str] = mapped_column(nullable=False)
