a886bc89853e5ddb8eace2b04586602dbafb77b7
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-12 17:17:15 +0200

    changed add -> upset_all, for update data if exists

--- src/infrastructure/db/repositories/event.py
+++ src/infrastructure/db/repositories/event.py
@@ -1,5 +1,6 @@
 import uuid as uuid_pkg
 from typing import Sequence
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import joinedload
 from sqlalchemy import select
 from .base import BaseRepo
@@ -8,7 +9,7 @@ from src.infrastructure.db.models.event import Event
 
 class EventRepo(BaseRepo):
 
-    async def get_events(self) -> Sequence[Event]:
+    async def get_all(self) -> Sequence[Event]:
         result = await self.session.execute(select(Event))
         return result.scalars().all()
 
@@ -18,8 +19,25 @@ class EventRepo(BaseRepo):
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def add(self, event: Event) -> None:
-        self.session.add(event)
+    async def upsert_all(self, events_data: list[dict]):
+        if not events_data:
+            return
+
+        stmt = insert(Event).values(events_data)
+
+        stmt = stmt.on_conflict_do_update(
+            index_elements=[Event.id],
+            set_={
+                "name": stmt.excluded.name,
+                "event_time": stmt.excluded.event_time,
+                "registration_deadline": stmt.excluded.registration_deadline,
+                "status": stmt.excluded.status,
+                "number_of_visitors": stmt.excluded.number_of_visitors,
+                "place_uuid": stmt.excluded.place_uuid,
+                "changed_at": stmt.excluded.changed_at,
+            },
+        )
+        await self.session.execute(stmt)
 
     async def delete(self, event: Event) -> None:
         await self.session.delete(event)
