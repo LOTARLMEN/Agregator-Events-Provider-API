cb3e108ec686c1c3c6c4238dbe02c2a0c020e395
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-12 17:18:21 +0200

    changed add -> upset_all

--- src/infrastructure/db/repositories/place.py
+++ src/infrastructure/db/repositories/place.py
@@ -2,13 +2,15 @@ import uuid as uuid_pkg
 from typing import Sequence
 
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
+
 from .base import BaseRepo
 from src.infrastructure.db.models.place import Place
 
 
 class PlaceRepo(BaseRepo):
 
-    async def get_places(self) -> Sequence[Place]:
+    async def get_all(self) -> Sequence[Place]:
         result = await self.session.execute(select(Place))
         return result.scalars().all()
 
@@ -18,8 +20,22 @@ class PlaceRepo(BaseRepo):
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def add(self, place: Place) -> None:
-        self.session.add(place)
+    async def upsert_all(self, places_data: list[dict]):
+        if not places_data:
+            return
+
+        stmt = insert(Place).values(places_data)
+
+        stmt = stmt.on_conflict_do_update(
+            index_elements=[Place.id],
+            set_={
+                "name": stmt.excluded.name,
+                "city": stmt.excluded.city,
+                "address": stmt.excluded.address,
+                "seats_pattern": stmt.excluded.seats_pattern,
+            },
+        )
+        await self.session.execute(stmt)
 
     async def delete(self, place: Place) -> None:
         await self.session.delete(place)
