d443c7f62f05567b98188754961a3ed4110d15ff
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-14 18:59:27 +0200

    add imports

--- src/presentation/api/rest/__init__.py
+++ src/presentation/api/rest/__init__.py
@@ -1,8 +1,7 @@
-__all__ = (
-    "sync_router",
-    "events_router",
-)
+__all__ = ("sync_router", "events_router", "health_router", "ticket_router")
 
 
 from .v1.controllers.sync import router as sync_router
 from .v1.controllers.events import router as events_router
+from .v1.controllers.health_check import router as health_router
+from .v1.controllers.ticket import router as ticket_router
