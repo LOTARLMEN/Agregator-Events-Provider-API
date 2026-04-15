c70a5772556b4ea1023385b673138c6ecad18b3d
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 16:02:39 +0200

    create router ticket

---
+++ src/presentation/api/rest/v1/controllers/ticket.py
@@ -0,0 +1,23 @@
+import uuid
+
+from fastapi import APIRouter, status, Depends
+from src.application.dtos.ticket import TicketResponseSchem, TicketRequestSchem
+from src.infrastructure.di import TicketRegUseCaseDep
+
+router = APIRouter(prefix="/api/tickets", tags=["Билеты"])
+
+
+@router.post(
+    "", status_code=status.HTTP_201_CREATED, response_model=TicketResponseSchem
+)
+async def register_ticket(
+    usecase: TicketRegUseCaseDep, ticket: TicketRequestSchem = Depends()
+) -> TicketResponseSchem:
+    return await usecase.reg_ticket(ticket)
+
+
+@router.delete("/{ticket_id}", status_code=status.HTTP_202_ACCEPTED)
+async def delete_ticket(
+    ticket_id: uuid.UUID, usecase: TicketRegUseCaseDep
+) -> dict[str, bool]:
+    return await usecase.del_ticket(ticket_id)
