290c83fbcf73efd1edbfc53100c0ca429a13171e
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 16:00:25 +0200

    add routers

--- src/presentation/api/rest/router.py
+++ src/presentation/api/rest/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from . import sync_router
-from . import events_router
+from . import sync_router, events_router, health_router, ticket_router
 
 
 router = APIRouter()
 
 router.include_router(sync_router)
+router.include_router(health_router)
 router.include_router(events_router)
+router.include_router(ticket_router)
