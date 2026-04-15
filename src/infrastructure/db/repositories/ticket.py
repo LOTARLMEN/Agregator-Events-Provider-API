c0a994c2b4ff5eca0cb1b65ff4cce2635ba5245f
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 16:02:28 +0200

    fixed ticket repo method add

--- src/infrastructure/db/repositories/ticket.py
+++ src/infrastructure/db/repositories/ticket.py
@@ -1,7 +1,10 @@
+import uuid
 import uuid as uuid_pkg
 from typing import Sequence
 
-from sqlalchemy import select
+from sqlalchemy import select, delete
+
+from src.application.dtos.ticket import TicketRequestSchem
 from .base import BaseRepo
 from src.infrastructure.db.models.ticket import Ticket
 
@@ -18,8 +21,30 @@ class TicketRepo(BaseRepo):
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def add(self, place: Ticket) -> None:
-        self.session.add(place)
+    async def add(
+        self, ticket_data: TicketRequestSchem, ticket_id: uuid.UUID
+    ) -> Ticket:
+        new_ticket = Ticket(
+            id=ticket_id,
+            event_id=ticket_data.event_id,
+            first_name=ticket_data.first_name,
+            last_name=ticket_data.last_name,
+            email=ticket_data.email,
+            seat=ticket_data.seat,
+        )
+        self.session.add(new_ticket)
+        return new_ticket
 
-    async def delete(self, place: Ticket) -> None:
-        await self.session.delete(place)
+    async def delete(self, ticket_id: uuid_pkg.UUID) -> bool:
+        stmt = delete(Ticket).where(Ticket.id == ticket_id)
+        result = await self.session.execute(stmt)
+        return result.rowcount > 0
+
+    async def is_ticket_in(self, ticket: TicketRequestSchem) -> bool:
+        query = select(Ticket).where(
+            Ticket.event_id == ticket.event_id, Ticket.seat == ticket.seat
+        )
+        result = await self.session.execute(query)
+        found_ticket = result.scalars().first()
+
+        return found_ticket is not None
