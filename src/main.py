dd673ded42fa40ffb719009c42f8cb14617ed00b
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 15:58:51 +0200

    add title, lifespan

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 from src.presentation.api.rest.router import router
+from src.presentation.api.lifespan import lifespan
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan, title="Event Aggregator API")
 app.include_router(router)
 
 
