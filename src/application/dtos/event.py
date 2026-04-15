eda9ce9c2e176f861085a41613d7d989cc69c2da
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 15:56:22 +0200

    add new classes

--- src/application/dtos/event.py
+++ src/application/dtos/event.py
@@ -3,7 +3,7 @@ from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict, Field
 
-from src.application.dtos.place import PlaceResponseSchema
+from src.application.dtos.place import PlaceResponseSchema, DetailPlaceResponseSchema
 
 
 class BaseEventSchema(BaseModel):
@@ -11,18 +11,38 @@ class BaseEventSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
-class EventResponseSchema(BaseEventSchema):
-
+class BaseEventResponseSchema(BaseEventSchema):
     id: UUID
     name: str
-    place: PlaceResponseSchema
     event_time: datetime
     registration_deadline: datetime
     status: str
     number_of_visitors: int
 
 
+class EventDetailResponseSchema(BaseEventResponseSchema):
+
+    place: DetailPlaceResponseSchema
+
+
+class EventResponseSchema(BaseEventResponseSchema):
+
+    place: PlaceResponseSchema
+
+
+class EventsResponseSchem(BaseEventSchema):
+    count: int
+    next: str | None
+    previous: str | None
+    results: list[EventResponseSchema]
+
+
 class EventsSeatsResponseSchema(BaseEventSchema):
 
     id: UUID = Field(alias="event_id")
+    available_seats: list[str]
+
+
+class EventSeatsResponseSchem(BaseModel):
+    event_id: UUID
     available_seats: list[str]
