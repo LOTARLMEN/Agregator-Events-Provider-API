bc020f99fbdaa9c40df2f63158ef979320504ca7
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-15 16:00:48 +0200

    add name tags

--- src/presentation/api/rest/v1/controllers/sync.py
+++ src/presentation/api/rest/v1/controllers/sync.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 from src.infrastructure import di as dep
 
 
-router = APIRouter(prefix="/api/sync")
+router = APIRouter(prefix="/api/sync", tags=["Ручная синхронизация"])
 
 
 @router.post("/trigger")
-async def sync_trigger(usecae: dep.AddUseCaseDep) -> dict:
-    await usecae.execute()
+async def sync_trigger(usecase: dep.AddUseCaseDep) -> dict:
+    await usecase.execute()
     return {"success": True}
