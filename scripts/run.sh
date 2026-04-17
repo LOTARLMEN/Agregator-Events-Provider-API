3223fdfb166cba800ec0c8034b81c5bf2ea39892
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 14:54:54 +0200

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "--- Synchronizing Database Schema State ---"
 # Мы сразу штампуем голову (head), это и есть наш файл 8e5245334c49
-uv run alembic stamp head
+uv run alembic upgrade head
 
 echo "--- Starting Uvicorn ---"
 exec uv run uvicorn src.main:app --host 0.0.0.0 --port 8000
