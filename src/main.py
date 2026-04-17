4043f5bfd1670a365c8fd9575e20f4f1821cf4a9
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 12:02:48 +0200

    add all handlers

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
 
-from src.presentation.api.rest.handlers import validation_exception_handler
+from src.presentation.api.rest.handlers import handlers_mapping
 
 from src.presentation.api.rest.router import router
 from src.presentation.api.lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan, title="Event Aggregator API")
-app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+
+for exc, handler in handlers_mapping.items():
+    app.add_exception_handler(exc, handler)
 app.include_router(router)
 
 
