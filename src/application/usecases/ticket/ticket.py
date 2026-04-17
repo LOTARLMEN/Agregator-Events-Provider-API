abe376022b92d3fcfc9b16ca9bc9bc2eb63c71fd
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 12:06:26 +0200

    fix excs

--- src/application/usecases/ticket/ticket.py
+++ src/application/usecases/ticket/ticket.py
@@ -1,63 +1,54 @@
 import uuid
 from datetime import datetime, timezone
 
-from httpx import ConnectTimeout, HTTPStatusError
-
 from src.application.dtos.ticket import TicketRequestSchem, TicketResponseSchem
-from src.application.usecases.base import BaseUseCase
-from src.infrastructure.db.exeptions import TicketIsRegisteredException, SeatException
+from src.application.exceptions import (
+    EventNotFound,
+    RegistrationDeadlinePasses,
+    TicketIsRegistered,
+    SeatNotAvailable,
+    TicketIsNotRegistered,
+    EventNotPublished,
+)
 
-from fastapi import status, HTTPException
+from src.application.usecases.base import BaseUseCase
+from src.infrastructure.db.models.event.status import EventStatus
 
 
 class TicketRegUseCase(BaseUseCase):
     async def reg_ticket(self, ticket: TicketRequestSchem) -> TicketResponseSchem:
         async with self.uow:
             event = await self.uow.events_repo.get_by_uuid(ticket.event_id)
-            if not event or event.status != "published":
-                raise HTTPException(
-                    status_code=400, detail="Event not published or not found"
-                )
+
+            if not event:
+                raise EventNotFound("Event not found.")
+
+            if event.status != EventStatus.PUBLISHED:
+                raise EventNotPublished("Event not published.")
 
             if (
                 event.registration_deadline
                 and datetime.now(timezone.utc) > event.registration_deadline
             ):
-                raise HTTPException(
-                    status_code=400, detail="Registration deadline has passed"
-                )
+                raise RegistrationDeadlinePasses("Registration deadline.")
 
             if await self.uow.ticket_repo.is_ticket_in(ticket):
-                raise TicketIsRegisteredException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail="This seat is already booked in our system",
-                )
+                raise TicketIsRegistered("Ticket already registered.")
 
-            try:
-                available_seats = await self.client.seats(ticket.event_id)
-                if ticket.seat not in available_seats["seats"]:
-                    raise SeatException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail=f"Seat {ticket.seat} is not available at provider",
-                    )
+            available_seats = await self.client.seats(ticket.event_id)
 
-                user_data = {
-                    "first_name": ticket.first_name,
-                    "last_name": ticket.last_name,
-                    "email": ticket.email,
-                    "seat": ticket.seat,
-                }
+            if ticket.seat not in available_seats["seats"]:
+                raise SeatNotAvailable("Seat not available.")
 
-                provider_res = await self.client.register(ticket.event_id, user_data)
-                provider_ticket_id = provider_res["ticket_id"]
+            user_data = {
+                "first_name": ticket.first_name,
+                "last_name": ticket.last_name,
+                "email": ticket.email,
+                "seat": ticket.seat,
+            }
 
-            except ConnectTimeout:
-                raise HTTPException(status_code=504, detail="Provider server timeout")
-            except HTTPStatusError as e:
-                raise HTTPException(
-                    status_code=e.response.status_code,
-                    detail=f"Provider error: {e.response.text}",
-                )
+            provider_res = await self.client.register(ticket.event_id, user_data)
+            provider_ticket_id = provider_res["ticket_id"]
 
             new_ticket = await self.uow.ticket_repo.add(
                 ticket, ticket_id=provider_ticket_id
@@ -70,18 +61,11 @@ class TicketRegUseCase(BaseUseCase):
     async def del_ticket(self, ticket_id: uuid.UUID) -> dict[str, bool]:
         async with self.uow:
             ticket = await self.uow.ticket_repo.get_by_uuid(ticket_id)
-            if not ticket:
-                raise HTTPException(status_code=404, detail="Ticket not found")
 
-            try:
-                await self.client.unregister(
-                    event_id=ticket.event_id, ticket_id=ticket.id
-                )
-            except HTTPStatusError as e:
-                raise HTTPException(
-                    status_code=e.response.status_code,
-                    detail=f"Provider error during unregister: {e.response.text}",
-                )
+            if not ticket:
+                raise TicketIsNotRegistered("Ticket not registered.")
+
+            await self.client.unregister(event_id=ticket.event_id, ticket_id=ticket.id)
 
             await self.uow.ticket_repo.delete(ticket_id)
             await self.uow.commit()
