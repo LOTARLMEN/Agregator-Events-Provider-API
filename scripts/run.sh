702b8b685680ffc455a5b893061d37120393f9f8
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 13:20:06 +0200

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-uv run alembic -c src/migration/alembic.ini  -x db_url="postgresql://${DB_USER}:${DB_PASS}@db:5432/${DB_NAME}" \
-  run_sql "DROP TABLE IF EXISTS alembic_version CASCADE;" || echo "SQL failed, but moving on..."
-
 MIGRATIONS_DIR="src/migration/versions"
 
 if [ -z "$(ls -A $MIGRATIONS_DIR/*.py 2>/dev/null)" ]; then
