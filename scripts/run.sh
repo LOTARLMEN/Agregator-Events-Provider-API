97f362c879ce31a47a4ec1cf3839bcf33256fc0a
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 12:45:10 +0200

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+DB_URL="postgresql://${DB_USER}:${DB_PASS}@db:5432/${DB_NAME}"
+
+python3 -c "from sqlalchemy import create_engine; engine = create_engine('$DB_URL'); engine.connect().execute('DROP TABLE IF EXISTS alembic_version CASCADE')"
+
 MIGRATIONS_DIR="src/migration/versions"
 
 if [ -z "$(ls -A $MIGRATIONS_DIR/*.py 2>/dev/null)" ]; then
