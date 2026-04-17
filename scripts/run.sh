137a7d4fb62dbe36fdec9571e537d9db393c5540
lotarlmen <val.janushkewitch@yandex.ru>
2026-04-17 14:08:12 +0200

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-# 1. Помечаем призрак (на всякий случай, если база чистая)
-uv run alembic stamp 3c4feebf7ad8
+# 1. Даем себе права на запись в папку миграций.
+# Без этого 'alembic revision' упадет с Permission denied.
+chmod -R 777 src/migration/versions
 
-# 2. Помечаем твою основную миграцию как ВЫПОЛНЕННУЮ.
-# Мы не используем upgrade, чтобы он не пытался снова создать таблицу "places".
-# Stamp просто запишет ID 8e5245334c49 в таблицу alembic_version.
-echo "Stamping main migration as already done..."
-uv run alembic stamp 8e5245334c49
+echo "--- ГЕНЕРИРУЕМ МИГРАЦИЮ ВНУТРИ КОНТЕЙНЕРА ---"
+# Генерируем файл миграции, который сравнит модели с базой и создаст недостающее (типы, таблицы)
+uv run alembic revision --autogenerate -m "auto_fix_from_k8s"
 
-# 3. Запуск сервера
-echo "Starting app..."
+echo "--- ПРИМЕНЯЕМ ---"
+uv run alembic upgrade head
+
+echo "--- ЗАПУСК ПРИЛОЖЕНИЯ ---"
 exec uv run uvicorn src.main:app --host 0.0.0.0 --port 8000
