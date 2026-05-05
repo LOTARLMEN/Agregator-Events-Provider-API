ce20520ad10b46b9d51b4fa60b0efe25d61fceb1
lotarlmen <val.janushkewitch@yandex.ru>
2026-05-05 17:33:19 +0200

    fixed name

--- src/infrastructure/clients/events/provider.py
+++ src/infrastructure/clients/events/provider.py
@@ -10,7 +10,7 @@ from src.config.config import setting
 class EventProviderClient:
     def __init__(self):
         self.base_url = setting.EVENTS_PROVIDER_BASE_URL
-        self.api_key = setting.EVENTS_PROVIDER_API_KEY
+        self.api_key = setting.X_API_KEY
 
         self.headers = {"x-api-key": self.api_key}
 
