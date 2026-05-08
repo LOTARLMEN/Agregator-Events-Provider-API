f0d7522e317bcf888c3c3a0fc03891113e51e96f
lotarlmen <val.janushkewitch@yandex.ru>
2026-05-08 10:43:33 +0200

    la grande finale fixe 6

--- src/infrastructure/clients/capashino/capashino.py
+++ src/infrastructure/clients/capashino/capashino.py
@@ -21,12 +21,17 @@ class CapashinoClient:
             # print(__name__, response.text)
             # print(__name__, response.request)
             # print(__name__, response.json())
+            print(
+                __name__,
+            )
             async with httpx.AsyncClient(follow_redirects=True, timeout=5) as client:
                 response = await client.post(
                     url,
                     headers=self.__headers,
                     json=payload,
                 )
+                print(response.status_code)
             return response.json()
         except Exception as e:
             print(__name__, e)
+            print(response.status_code or 0)
