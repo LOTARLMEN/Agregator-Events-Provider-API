a9f80a2ae3b7d8df3ef46afeb2715650b2a66438
lotarlmen <val.janushkewitch@yandex.ru>
2026-05-18 15:35:45 +0200

    add news depends on metrics

--- src/infrastructure/di.py
+++ src/infrastructure/di.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import src.application.usecases as usc
 from src.infrastructure.clients.events.provider import EventProviderClient
+from src.infrastructure.db.repositories.event import EventRepo
+from src.infrastructure.db.repositories.ticket import TicketRepo
 from src.infrastructure.db.session import get_async_session
 from src.infrastructure.db.uow import UnitOfWork
 
@@ -20,6 +22,14 @@ def get_event_provider_client() -> EventProviderClient:
 
 def get_uow(session: SessionDep) -> UnitOfWork:
     return UnitOfWork(session)
+
+
+def get_event_repo(session: SessionDep):
+    return EventRepo(session)
+
+
+def get_ticket_repo(session: SessionDep):
+    return TicketRepo(session)
 
 
 ClientDep = Annotated[EventProviderClient, Depends(get_event_provider_client)]
@@ -41,3 +51,7 @@ def get_reg_ticket_usecase(uow: UoWDep, client: ClientDep) -> usc.TicketRegUseCa
 AddUseCaseDep = Annotated[usc.AddEventsUseCase, Depends(get_add_event_usecase)]
 GetUseCaseDep = Annotated[usc.GetEventsUseCase, Depends(get_event_usecase)]
 TicketRegUseCaseDep = Annotated[usc.TicketRegUseCase, Depends(get_reg_ticket_usecase)]
+
+
+EventRepoDep = Annotated[EventRepo, Depends(get_event_repo)]
+TicketRepoDep = Annotated[TicketRepo, Depends(get_ticket_repo)]
