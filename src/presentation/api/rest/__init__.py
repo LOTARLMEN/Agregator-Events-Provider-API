e3127d23bfa9281fbbca3a3294f9a86d93966be4
lotarlmen <val.janushkewitch@yandex.ru>
2026-05-18 15:37:03 +0200

    add router

--- src/presentation/api/rest/__init__.py
+++ src/presentation/api/rest/__init__.py
@@ -4,11 +4,13 @@ __all__ = (
     "events_router",
     "health_router",
     "ticket_router",
+    "metrics_router",
 )
 
 
 from .v1.controllers.events import router as events_router
 from .v1.controllers.health_check import router as health_router
+from .v1.controllers.metrics import router as metrics_router
 from .v1.controllers.sentry_check import router as sentry_check_router
 from .v1.controllers.sync import router as sync_router
 from .v1.controllers.ticket import router as ticket_router
