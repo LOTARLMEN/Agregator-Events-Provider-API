42ad88851882a5ecc474975e3f78caf9ddd475ea
lotarlmen <val.janushkewitch@yandex.ru>
2026-05-18 15:36:07 +0200

    create prometheus_client metrics

---
+++ src/infrastructure/metrics.py
@@ -0,0 +1,43 @@
+from prometheus_client import Counter, Gauge, Histogram
+
+http_requests_total = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests",
+    ["method", "endpoint", "status"],
+)
+
+http_request_duration_seconds = Histogram(
+    "http_request_duration_seconds",
+    "Duration of HTTP requests",
+    [
+        "method",
+        "endpoint",
+    ],
+    buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0],
+)
+
+events_provider_requests_total = Counter(
+    "events_provider_requests_total",
+    "Total number of events providers requests",
+    ["endpoint", "status"],
+)
+
+events_provider_request_duration_seconds = Histogram(
+    "events_provider_request_duration_seconds",
+    "Duration of events providers requests",
+    ["endpoint"],
+    buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0],
+)
+
+
+events_total = Gauge("events_total", "Total number of events in database")
+tickets_created_total = Gauge(
+    "tickets_created_total", "Total number of tickets in database"
+)
+tickets_cancelled_total = Gauge(
+    "tickets_cancelled_total", "Total number of cancelled tickets in database"
+)
+
+
+cache_hits_total = Counter("cache_hits_total", "Total number of hits in cache")
+cache_misses_total = Counter("cache_misses_total", "Total number of misses in cache")
